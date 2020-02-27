831ed5e08c2c5010cff534ba0ffb09d508040df3
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-27 09:27:53 +0530

    MODIFIED: Fliping coin and print head or tail

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 #PRINTING MESSAGE
 printf "Welcome to flip coin simulation \n"
+#GENERATING RANDOM VALUE
+result=$((RANDOM%2+1))
+isHead=1
+#CONDITION IS TRUE THEN HEAD OTHERWISE TAIL
+if(( $isHead == $result))
+then
+	echo "Head"
+else
+	echo "Tail"
+fi
