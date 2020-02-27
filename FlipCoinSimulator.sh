5a2ce75e3136ee5ec1acccea0feaa24ffb6639d9
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-27 10:19:35 +0530

    MODIFIED: Fliping coin multiple time and finding percentage

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -11,51 +11,82 @@ TEMP=$userInput
 #INITILIZE DICTIONARY
 declare -A FlipingCoin
 #CONSTANT VARIBALE
-COUNT_HH=0		#HH=Head Head
-COUNT_HT=0		#HT=Head Tail
-COUNT_TT=0		#TT=Tail Tail
-COUNT_TH=0		#TH=Tail Tail
-#INITILIZE DICTIONARY
-declare -A FlipingCoin
+COUNT_HHH=0		#HHH=Head Head Head
+COUNT_HHT=0		#HHT=Head Head Tail
+COUNT_HTH=0		#HTH=Head Tail Head
+COUNT_THH=0		#THH=Tail Head Head
+COUNT_TTT=0		#TTT=Tail Tail Tail
+COUNT_TTH=0		#TTH=Tail Tail Head
+COUNT_THT=0		#THT=Tail Head Tail
+COUNT_HTT=0		#HTT=Head Tail Tail
 #USING LOOP TO FLIP COIN MULTIPLE TIME
 for(( i=1; i<=$userInput; i++ ))
-	do
+do
 	#GENERATING RANDOM VALUE
-	result=$((RANDOM%4+1))
-	#USINGCASESTATEMENT
+	result=$((RANDOM%8+1))
+		#USING CASE STATEMENT
 		case $result in
 			1)
-				COUNT_HH=$(($COUNT_HH+1))
-				FlipingCoin["HH"]="$COUNT_HH"
+				COUNT_HHH=$(($COUNT_HHH+1))
+				FlipingCoin["HHH"]="$COUNT_HHH"
 				;;
+
 			2)
-				COUNT_HT=$(($COUNT_HT+1))
-				FlipingCoin["HT"]="$COUNT_HT"
+				COUNT_HHT=$(($COUNT_HHT+1))
+				FlipingCoin["HHT"]="$COUNT_HHT"
 				;;
+
 			3)
-				COUNT_TT=$(($COUNT_TT+1))
-				FlipingCoin["TT"]="$COUNT_TT"
+   		   COUNT_HTH=$(($COUNT_HTH+1))
+				FlipingCoin["HTH"]="$COUNT_HTH"
 				;;
+
 			4)
-				COUNT_TH=$(($COUNT_TH+1))
-				FlipingCoin["TH"]="$COUNT_TH"
+				COUNT_THH=$(($COUNT_THH+1))
+				FlipingCoin["THH"]="$COUNT_THH"
 				;;
-			esac
-	done
+
+			5)
+				COUNT_TTT=$(($COUNT_TTT+1))
+				FlipingCoin["TTT"]="$COUNT_TTT"
+				;;
+
+			6)
+				COUNT_TTH=$(($COUNT_TTH+1))
+				FlipingCoin["TTH"]="$COUNT_TTH"
+				;;
+
+			7)
+				COUNT_THT=$(($COUNT_THT+1))
+				FlipingCoin["THT"]="$COUNT_THT"
+				;;
+
+			8)
+				COUNT_HTT=$(($COUNT_HTT+1))
+				FlipingCoin["HTT"]="$COUNT_HTT"
+				;;
+		esac
+done
 #CALCULATING PERSENTAGE OF FLIPING COIN
-headHeadPercentage=`echo "scale=3; $COUNT_HH / $TEMP * 100" |bc -l`
-headTailPercentage=`echo "scale=3; $COUNT_HT / $TEMP * 100" |bc -l`
-tailTailPercentage=`echo "scale=3; $COUNT_TT / $TEMP * 100" |bc -l`
-tailHeadPercentage=`echo "scale=3; $COUNT_TH / $TEMP * 100" |bc -l`
-#PRINTING KEY AND VALUE
+hhhPercentage=`echo "scale=3; $COUNT_HHH / $TEMP * 100" |bc -l`
+hhtPercentage=`echo "scale=3; $COUNT_HHT / $TEMP * 100"  |bc -l`
+hthPercentage=`echo "scale=3; $COUNT_HTH / $TEMP * 100"  |bc -l`
+thhPercentage=`echo "scale=3; $COUNT_THH / $TEMP * 100"  |bc -l`
+tttPercentage=`echo "scale=3; $COUNT_TTT / $TEMP * 100"  |bc -l`
+tthPercentage=`echo "scale=3; $COUNT_TTH / $TEMP * 100"  |bc -l`
+thtPercentage=`echo "scale=3; $COUNT_THT / $TEMP * 100"  |bc -l`
+httPercentage=`echo "scale=3; $COUNT_HTT / $TEMP * 100"  |bc -l`
+#PRINTING NUMBER OF TIME HEAD AND TAIL
 for count in ${!FlipingCoin[@]}
-do
+	do
 	echo "$count : ${FlipingCoin[$count]}"
 done
-#USING DICTIONARY TO PRINT RESULT OF FILPING COIN
-echo "Head head percentage : $headHeadPercentage"
-echo "Head tail percentage : $headTailPercentage"
-echo "Tail tail percentage : $tailTailPercentage"
-echo "Tail head percentage : $tailHeadPercentage"
-
-
+#PRINT RESULT OF FILPING COIN
+echo "Head head head percentage : $hhhPercentage"
+echo "Head head tail percentage : $hhtPercentage"
+echo "Head tail head percentage : $hthPercentage"
+echo "Tail head head percentage : $thhPercentage"
+echo "Tail tail tail percentage : $tttPercentage"
+echo "Tail tail head percentage : $tthPercentage"
+echo "Tail head tail percentage : $thtPercentage"
+echo "Head Tail tail percentage : $httPercentage"
